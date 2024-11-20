12d47d0a3a65cf2c8e0d1d759159dd68005b6cd4
Ayc023 <Dakiesayco@gmail.com>
2024-11-20 09:58:46 +0800

    added changes

--- checkout.php
+++ checkout.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+echo "Welcome to the checkout page!";
+
 // Include database connection file
 require_once 'config.php';
 
